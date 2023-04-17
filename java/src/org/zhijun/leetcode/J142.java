358641e345ba2480420d9b20654e5e8998147238
xiangzhijun <xiangzhijun@bixin.cn>
2023-04-17 22:51:34 +0800

    [142]环形链表II

---
+++ java/src/org/zhijun/leetcode/J142.java
@@ -0,0 +1,110 @@
+package org.zhijun.leetcode;
+
+//[142]环形链表II
+//给定一个链表的头节点 head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+//
+// 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到
+//链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+//
+// 不允许修改 链表。
+//
+//
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：head = [3,2,0,-4], pos = 1
+//输出：返回索引为 1 的链表节点
+//解释：链表中有一个环，其尾部连接到第二个节点。
+//
+//
+// 示例 2：
+//
+//
+//
+//
+//输入：head = [1,2], pos = 0
+//输出：返回索引为 0 的链表节点
+//解释：链表中有一个环，其尾部连接到第一个节点。
+//
+//
+// 示例 3：
+//
+//
+//
+//
+//输入：head = [1], pos = -1
+//输出：返回 null
+//解释：链表中没有环。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点的数目范围在范围 [0, 10⁴] 内
+// -10⁵ <= Node.val <= 10⁵
+// pos 的值为 -1 或者链表中的一个有效索引
+//
+//
+//
+//
+// 进阶：你是否可以使用 O(1) 空间解决此题？
+//
+// Related Topics 哈希表 链表 双指针 👍 2071 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class J142 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (true) {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                //第一次相遇，假设fast走了f步，slow走了s步；
+                //f=2s
+                //f=s+nb;b为环的长度，fast比slow多走了n圈；
+                //由上述两个得s=nb，即slow走了n圈；f=2s=2nb，fast走了2n圈；
+                //假设环入口位置为a，那么一个指针走动入口一定是a+nb；
+                //那边slow已经走了nb，只需要再走a步即可；
+                //所以将fast重置为head，后面fast和slow每次都只走一步；
+                //fast再走a步到达入口，此时slow一共走了a+nb也到达入口，即第二次fast和slow相遇的地方就是入口；
+                break;
+            }
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+
+//    public static class ListNode {
+//        int val;
+//        ListNode next;
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
