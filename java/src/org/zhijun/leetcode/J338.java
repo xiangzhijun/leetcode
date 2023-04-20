c88a2a307dea63e41ffd25d483dafa158765d2c2
xiangzhijun <xiangzhijun@bixin.cn>
2023-04-20 22:34:55 +0800

    [338]比特位计数

---
+++ java/src/org/zhijun/leetcode/J338.java
@@ -0,0 +1,79 @@
+package org.zhijun.leetcode;
+
+
+//[338]比特位计数
+//给你一个整数 n ，对于 0 <= i <= n 中的每个 i ，计算其二进制表示中 1 的个数 ，返回一个长度为 n + 1 的数组 ans 作为答案。
+//
+//
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：n = 2
+//输出：[0,1,1]
+//解释：
+//0 --> 0
+//1 --> 1
+//2 --> 10
+//
+//
+// 示例 2：
+//
+//
+//输入：n = 5
+//输出：[0,1,1,2,1,2]
+//解释：
+//0 --> 0
+//1 --> 1
+//2 --> 10
+//3 --> 11
+//4 --> 100
+//5 --> 101
+//
+//
+//
+//
+// 提示：
+//
+//
+// 0 <= n <= 10⁵
+//
+//
+//
+//
+// 进阶：
+//
+//
+// 很容易就能实现时间复杂度为 O(n log n) 的解决方案，你可以在线性时间复杂度 O(n) 内用一趟扫描解决此问题吗？
+// 你能不使用任何内置函数解决此问题吗？（如，C++ 中的 __builtin_popcount ）
+//
+//
+// Related Topics 位运算 动态规划 👍 1193 👎 0
+
+
+import java.util.Arrays;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class J338 {
+    public int[] countBits(int n) {
+        int[] rst = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            rst[i]++;
+            //i&i-1去除i最后一位1
+            rst[i] += rst[i & (i - 1)];
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        J338 solution = new J338();
+        System.out.println(Arrays.toString(solution.countBits(2)));
+        System.out.println(Arrays.toString(solution.countBits(5)));
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
